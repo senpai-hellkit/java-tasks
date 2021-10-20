822205017860e4fe45ea15b1ab9f7f8f5281d11a
unknown <nofexx@bk.ru>
2021-10-21 01:16:05 +0300

    I have done this hubList... I think thats greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,172 +1,108 @@
 package com.company;
 
+import java.util.Comparator;
+
+class HubList<E> implements HubListImpl<E> {
+    int size = 0;
+    E[] list = (E[]) new Object[size];
+
+    @Override
+    public void add(E element) {
+        this.size++;
+        E[] newList = (E[]) new Object[this.size];
+        for (int i = 0; i < this.list.length; i++) {
+            newList[i] = this.list[i];
+        }
+        newList[this.size-1] = element;
+        this.list = newList;
+    }
+
+    @Override
+    public E removeAt(int index) {
+        E[] newList = (E[]) new Object[this.size-1];
+        E item = null;
+        int count = 0;
+        if (index > this.size-1) {
+            return null;
+        }
+        for (int i = 0; i < this.size; i++) {
+            if (index == i) {
+                item = list[i];
+            }
+            if (index != i) {
+                newList[count] = list[i];
+                count++;
+            }
+            if (i == this.size-1) {
+                this.list = newList;
+                this.size--;
+                return item;
+            }
+        }
+        return item;
+    }
+
+    @Override
+    public void sort(Comparator<? super E> c) {
+        for (int i = 0; i < this.size - 1; i++) {
+            for (int j = this.size - 1; j > i; j--) {
+                if (c.compare(this.list[i], this.list[j]) > 0) {
+                    E temp = this.list[j - 1];
+                    this.list[j - 1] = this.list[j];
+                    this.list[j] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int find(E element) {
+        for (int i = 0; i < this.size; i++) {
+            if(this.list[i].equals(element)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void showData() {
+        for (E e : this.list) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+}
+
+class HubComparator implements Comparator<Object> {
+    @Override
+    public int compare(Object a, Object b) {
+        if (a instanceof Integer) {
+            return Integer.compare((int) a, (int) b);
+        } else {
+            return -1;
+        }
+    }
+}
+
+interface HubListImpl<E> {
+    void add(E element);
+    E removeAt(int index);
+    void sort(Comparator<? super E> c);
+    int find(E element);
+}
 
 public class Main {
     public static void main(String[] args) {
-        com.company.Matrix firstMatrix = new com.company.Matrix(3, 3);
-        System.out.println("Первая матрица");
-        firstMatrix.displayMatrix(); // вывод матрицы
-
-        com.company.Matrix secondMatrix = new com.company.Matrix(3,3);
-        System.out.println("Вторая матрица");
-        secondMatrix.displayMatrix();
-
-        System.out.println("Сложение матриц");
-        firstMatrix.sumWithNewMatrix(secondMatrix.getMatrix()); // сложение исходной матрицы с другой матрицей
-        System.out.println("Умножение матрицы на число");
-        firstMatrix.mullToEta(10); // умножение матрицы на число ета
-        firstMatrix.displayMatrix();
-
-        System.out.println("Умножение матриц");
-        double[][] result = com.company.Matrix.mullMatrix(firstMatrix.getMatrix(), secondMatrix.getMatrix()); // умножение матрицы на матрицу
-        com.company.Matrix.displayMatrix(result);
-
-        System.out.println("Прямоугольная матрица");
-        com.company.RectangleMatrix rectMatrix = new com.company.RectangleMatrix(3,3);
-        rectMatrix.displayMatrix();
-        double determinant = rectMatrix.determinant(rectMatrix.getMatrix());
-        System.out.printf("Определитель: %.3f", determinant);
+        HubList<Integer> youList = new HubList<Integer>();
+        youList.add(4);
+        youList.add(578);
+        youList.add(3);
+        youList.showData(); // [4, 578, 3]
+        System.out.println(youList.find(4)); // 0
+        System.out.println(youList.find(1)); // -1
+        youList.sort(new HubComparator()); // ...
+        youList.showData(); // [3, 4, 578]
+        System.out.println(youList.removeAt(2)); // 578
+        youList.showData(); // [3, 4]
     }
-}
-
-/*
-    Домашнее задание:
-
-        Создать класс "Матрица". +
-        Конструктор: на вход высота, ширина. Заполнение рандомом +
-
-    Класс должен иметь следующие поля:
-        двумерный массив вещественных чисел; +
-        количество строк и столбцов в матрице +
-
-    Класс должен иметь следующие методы:
-        сложение с другой матрицей; +
-        умножение на число; +
-        вывод на печать; +
-        статический метод умножения матрицы на матрицу. Возвращается третья матрица +
-
-    Сделать подкласс Прямоугольная матрица +
-    в конструктор передаётся размер. Заполнение рандомом +
-    метод - определитель +
- */
-
-class Matrix {
-    protected int n; // кол-во столбцов в матрице
-    protected int m; // кол-во строк в матрице
-    protected double [][] M; // матрица
-
-    Matrix (int m, int n) {
-        this.m = m;
-        this.n = n;
-        this.M = new double[m][n];
-        for (int i = 0; i < this.m; i++) {
-            for (int j = 0; j < this.n; j++) {
-                M[i][j] = Math.random() * 100;
-            }
-        }
-    }
-
-    void displayMatrix() {
-        for (int i = 0; i < this.m; i++) {
-            System.out.println();
-            System.out.print("|");
-            for (int j = 0; j < this.n; j++) {
-                System.out.printf(" %.3f", this.M[i][j]);
-            }
-            System.out.print(" |");
-        }
-        System.out.println();
-    }
-
-    static void displayMatrix(double[][] M) {
-        for (int i = 0; i < M.length; i++) {
-            System.out.println();
-            System.out.print("|");
-            for (int j = 0; j < M[0].length; j++) {
-                System.out.printf(" %.3f", M[i][j]);
-            }
-            System.out.print(" |");
-        }
-        System.out.println();
-    }
-
-    void sumWithNewMatrix(double[][] newM) {
-        if (newM[0].length != this.n || newM.length != this.m) {
-            throw new IllegalArgumentException(
-                    "Нельзя сложить матрицы! Начальная матрица: " + this.m + "x" + this.n +
-                            " А ваша: " + newM.length + "x" + newM[0].length);
-        }
-        double[][] result = new double[this.m][this.n];
-        for (int i = 0; i < this.m; i++) {
-            for (int j = 0; j < this.n; j++) {
-                result[i][j] = this.M[i][j] + newM[i][j];
-            }
-        }
-        displayMatrix(result);
-    }
-
-    void mullToEta(int eta) {
-        for (int i = 0; i < this.m; i++) {
-            for (int j = 0; j < this.n; j++) {
-                this.M[i][j] *= eta;
-            }
-        }
-    }
-
-    double[][] getMatrix() {
-        return this.M;
-    }
-
-    static public double[][] mullMatrix(double[][] A, double[][] B) {
-        if (A[0].length != B.length) {
-            throw new IllegalArgumentException(
-                    "Нельзя умножить матрицы! Начальная матрица: " + A[0].length + "x" + A.length +
-                            " Вторая: " + B.length + "x" + B[0].length + " т.е. " + A[0].length + "!=" + B.length);
-        }
-        double[][] result = new double[B.length][A[0].length];
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result.length; j++) {
-                for (int k = 0; k < result.length; k++) {
-                    result[i][j] += A[i][k] * B[k][j];
-                }
-            }
-        }
-        return result;
-    }
-
-}
-
-class RectangleMatrix extends com.company.Matrix {
-
-    RectangleMatrix(int m, int n) {
-        super(m, n);
-    }
-
-    double[][] minor(double[][] M, int i, int j) {
-        double[][] minor = new double[M.length-1][M.length-1];
-        for (int k = 0; k < M.length; k++) {
-            for (int l = 0; k != i && l < M[k].length; l++) {
-                if (l != j) {
-                    minor[(k < i) ? k : k - 1][(l < j) ? l : l - 1] = M[k][l];
-                }
-            }
-        }
-        return minor;
-    }
-
-    double determinant(double[][] M) {
-        if (M[0].length != M.length){
-            throw new IllegalArgumentException(
-                    "Невозможно найти определитель! Ваша матрица размером: MxN А должна быть NxN");
-        }
-        if (M.length == 2) {
-            return M[0][0] * M[1][1] - M[0][1] * M[1][0];
-        }
-        double determinant = 0;
-        for (int c = 0; c < M.length; c++) {
-            determinant += (Math.pow((-1), c) * M[0][c] * determinant(minor(M, 0, c)));
-        }
-        return determinant;
-    }
-
 }
