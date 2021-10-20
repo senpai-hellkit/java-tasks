9f1e56dff785d6fa877b7d4c9016a9ad72495dbc
unknown <nofexx@bk.ru>
2021-10-21 01:20:31 +0300

    I have done, this is greet task (>_<)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,108 +1,98 @@
 package com.company;
 
-import java.util.Comparator;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
 
-class HubList<E> implements HubListImpl<E> {
-    int size = 0;
-    E[] list = (E[]) new Object[size];
-
-    @Override
-    public void add(E element) {
-        this.size++;
-        E[] newList = (E[]) new Object[this.size];
-        for (int i = 0; i < this.list.length; i++) {
-            newList[i] = this.list[i];
-        }
-        newList[this.size-1] = element;
-        this.list = newList;
-    }
-
-    @Override
-    public E removeAt(int index) {
-        E[] newList = (E[]) new Object[this.size-1];
-        E item = null;
-        int count = 0;
-        if (index > this.size-1) {
-            return null;
-        }
-        for (int i = 0; i < this.size; i++) {
-            if (index == i) {
-                item = list[i];
-            }
-            if (index != i) {
-                newList[count] = list[i];
-                count++;
-            }
-            if (i == this.size-1) {
-                this.list = newList;
-                this.size--;
-                return item;
-            }
-        }
-        return item;
-    }
-
-    @Override
-    public void sort(Comparator<? super E> c) {
-        for (int i = 0; i < this.size - 1; i++) {
-            for (int j = this.size - 1; j > i; j--) {
-                if (c.compare(this.list[i], this.list[j]) > 0) {
-                    E temp = this.list[j - 1];
-                    this.list[j - 1] = this.list[j];
-                    this.list[j] = temp;
-                }
-            }
-        }
-    }
-
-    @Override
-    public int find(E element) {
-        for (int i = 0; i < this.size; i++) {
-            if(this.list[i].equals(element)){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    void showData() {
-        for (E e : this.list) {
-            System.out.print(e + " ");
-        }
-        System.out.println();
-    }
-}
-
-class HubComparator implements Comparator<Object> {
-    @Override
-    public int compare(Object a, Object b) {
-        if (a instanceof Integer) {
-            return Integer.compare((int) a, (int) b);
-        } else {
-            return -1;
-        }
-    }
-}
-
-interface HubListImpl<E> {
-    void add(E element);
-    E removeAt(int index);
-    void sort(Comparator<? super E> c);
-    int find(E element);
-}
 
 public class Main {
     public static void main(String[] args) {
-        HubList<Integer> youList = new HubList<Integer>();
-        youList.add(4);
-        youList.add(578);
-        youList.add(3);
-        youList.showData(); // [4, 578, 3]
-        System.out.println(youList.find(4)); // 0
-        System.out.println(youList.find(1)); // -1
-        youList.sort(new HubComparator()); // ...
-        youList.showData(); // [3, 4, 578]
-        System.out.println(youList.removeAt(2)); // 578
-        youList.showData(); // [3, 4]
+        Scanner input = new Scanner(System.in);
+        Auth auth = new Auth();
+        System.out.print("login: ");
+        String login = input.next();
+        auth.fillLogin(login);
+        System.out.print("code: ");
+        String code = input.next();
+        if (auth.checkCode(code)) {
+            System.out.println("Auth");
+        } else {
+            System.out.println("Not Auth");
+        }
+    }
+}
+/*
+    класс Auth
+    поля phone, email, code
+    методы
+    fillLogin(String login);
+    //TODO: login - телефон или email.
+    регулярка для телефона или мыла.
+    Если ок, записываем в поле
+    sendCode();  вызывает Coder().sendCode();
+    checkCode(String code);
+    //проверяем, что 6 символов.
+    вызывает Coder().checkCode(code);
+    class Coder
+    поля: приватное код
+    методы:
+    sendCode();
+    //генерируем код рандомом, сохраняем
+    checkCode(String code);
+    //проверяем, что он правильный
+ */
+class Coder {
+    int maxLength = 6;
+    private String code;
+    private static Random rnd = new Random();
+    public void sendCode() {
+        char nextChar;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 6; i++) {
+            nextChar = (char) (rnd.nextInt(26) + 97);
+            sb.append(nextChar);
+            if ((i + 1) % 5 == 0 && i != 5);
+        }
+        this.code = sb.toString();
+        writeToFile(this.code);
+    }
+    static void writeToFile(String str) {
+        try
+        {
+            FileWriter writer = new FileWriter("code.txt", false);
+            writer.write(str);
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    boolean checkCode(String code) {
+        return (code.length() == this.maxLength && this.code.equals(code));
+    }
+}
+class Auth {
+    String phone;
+    String email;
+    String chekEmail = "^\\w+\\.?\\w+?@\\w+\\.\\w*\\.*\\w*";
+    String checkPhone = "^[+]*[0-9]{1,4}[-\\s\\./0-9]*$";
+    Coder coder = new Coder();
+    void fillLogin(String login) {
+        if (login.matches(this.chekEmail)) {
+            this.sendCode();
+            this.email = login;
+        } else if (login.matches(this.checkPhone)) {
+            this.phone = login;
+            this.sendCode();
+        } else {
+            throw new IllegalArgumentException("Неверный логин");
+        }
+    }
+    void sendCode() {
+        this.coder.sendCode();
+    }
+    boolean checkCode(String code) {
+        return this.coder.checkCode(code);
     }
 }
