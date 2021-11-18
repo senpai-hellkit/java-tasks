ae579caa7ac01421a78a1f06377d2e120373d54b
unknown <nofexx@bk.ru>
2021-11-18 11:14:40 +0300

    good job, nice try

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,53 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
+/*
+Задание на эту пару. Написать любым способом прогу, которая будет:
+1. Выдавать случайного студента вашей группы
+2. Ждать оценку от 1 до 5 в консоль
+3. После выдавать следующего студента
 
+Также должен быть вывод всех студентов с оценкой при вводе в консоль "print"
+*/
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Auth auth = new Auth();
-        Server server = new Server();
+        ArrayList<String> students = new ArrayList<String>();
+        students.add("Вася");
+        students.add("Дима");
+        students.add("Леша");
+        students.add("Игорь");
+        students.add("Миша");
+        students.add("Рудик");
+        students.add("Тузик");
+        students.add("Боби");
+        students.add("Адам");
+        students.add("Джесси");
+        Scanner in = new Scanner(System.in);
+        Collections.shuffle(students);
+        int index;
+        String mark = "print";
+        while (!mark.equals("exit")) {
+            index = getIdStudent(students);
+            System.out.println(students.get(index));
+            System.out.print("Что поставим? : ");
+            mark = in.next();
+            logistic(mark, index, students);
+        }
 
-        System.out.print("login: ");
-        String login = input.next();
-        auth.fillLogin(login);
-        System.out.print("code: ");
-        String code = input.next();
-        if (server.verify(login, code)) {
-            server.auth(login, code);
+    }
+
+    static int getIdStudent(ArrayList<String> arr) {
+        return (int)Math.floor(Math.random() * arr.size());
+    }
+
+    static void logistic(String arg, int index, ArrayList<String> arr) {
+        if (arg.equals("print")) {
+            System.out.println(arr.toString());
         } else {
-            server.tryAgain();
+            arr.set(index, arr.get(index) + " " + arg);
         }
     }
 }
